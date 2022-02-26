9158c9f95d1054faeb6b9cd0466952409f588fb2
SweetTomato111 <chloe.yiu.yee.man@gmail.com>
2022-02-26 19:04:10 +0000

    Added comment

--- Barcode_webcam.py
+++ Barcode_webcam.py
@@ -9,8 +9,10 @@ data = None
 data0 = None
 
 while True:
+    # reset data
     data = None
-    
+
+    # decode image
     ret, image = cap.read()
     detectedBarcodes = decode(image)
 
@@ -28,21 +30,25 @@ while True:
             (x, y, w, h) = barcode.rect
             cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 5)
             cv2.putText(image, barcode.data.decode(), (x, y), cv2.FONT_HERSHEY_SIMPLEX,
-                    0.5, (0, 255, 0), 2) 
+                    0.5, (0, 0, 255), 2) 
     
+    # show video
     cv2.imshow("Video",image)
 
     if(cv2.waitKey(1) == ord("q")):
         break
     
-    # check if it is a link
     if data:
+        # if the scanned code is different
         if data != data0:
             print("Data found: ", data)
             print("Data type: ", barcode.type)
+            print('')
             data0 = data
+            # if the information from the code is a link
             if validators.url(data):
-                response = input("Do you want to open the link? (Y, YES): ")
+                response = input("Do you want to open the link? (Y/YES): ")
+                # reset variable so the code can be rescanned
                 data0 = None
                 if response.upper().strip() == 'Y' or response.upper().strip() == 'YES':
                     webbrowser.open(data, new=2)
