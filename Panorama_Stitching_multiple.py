9c77f6c5f7d86fc4c6614555a9fa45dab429eb4d
IdealistMatthew <matthewah1125@gmail.com>
2022-02-26 15:08:17 +0000

    small changes

--- Panorama_Stitching_multiple.py
+++ Panorama_Stitching_multiple.py
@@ -246,7 +246,7 @@ if __name__ == "__main__":
 
     current_time = time.time()
     initial_time = current_time
-    end_time = time.time() + 2
+    end_time = time.time() + 5
     img_array = []
 
     while current_time <= end_time:
@@ -277,7 +277,7 @@ if __name__ == "__main__":
     for i in range(1, len(Images)):
         StitchedImage = StitchImages(BaseImage, Images[i])
         BaseImage = StitchedImage.copy()
-        print("Compilith image number {}".format(i))
+        print("Compiling image number {}".format(i))
 
     cv2.imwrite("Stitched_Panorama.png", BaseImage)
     cv2.imshow("Stitched_Panorama.png", BaseImage)
